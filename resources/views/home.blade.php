f54971654119fa7ef6c485cf4ee4650b54fa8300
system-intelligence <system.intelligence2025@gmail.com>
2025-09-22 08:54:13 +0800

    alchy app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+
+<x-layout>
+   <x-slot:title>
+       Welcome
+   </x-slot:title>
+
+   <div class="max-w-2xl mx-auto">
+       <div class="card bg-base-100 shadow mt-8">
+           <div class="card-body">
+               <div>
+                   <h1 class="text-3xl font-bold">Welcome to Alchy!</h1>
+                   <p class="mt-4 text-base-content/60">Inventory management made simple. Start by creating your first item and keep track of your stock with ease. Happy inventorying!</p>
+                       </p>
+               </div>
+           </div>
+       </div>
+   </div>
+</x-layout>
